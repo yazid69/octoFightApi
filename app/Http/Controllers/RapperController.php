c6773b6a78f27453ce21854e0a31c496267c5f6c
Luluww 👽 <detplucas@gmail.com>
2024-10-14 14:56:31 +0200

    ajout getrapper

--- app/Http/Controllers/RapperController.php
+++ app/Http/Controllers/RapperController.php
@@ -31,4 +31,32 @@ class RapperController extends Controller
 
         return response()->json($rappers);
     }
+
+    public function getRapper($id)
+    {
+        $maxAttack = 100;
+        $maxDefense = 100;
+
+        $rapper = Rapper::find($id);
+
+        if (!$rapper) {
+            return response()->json(['error' => 'Rappeur non trouvé'], 404);
+        }
+
+        $attaque = ($rapper->popularity * 1.1) + ($rapper->followers / 1500000); 
+        $attaque = min($attaque, $maxAttack);
+
+        $defense = ($rapper->followers * 0.00003) + ($rapper->popularity * 0.2);
+        $defense = min($defense, $maxDefense);
+
+        return response()->json([
+            'id' => $rapper->id,
+            'name' => $rapper->name,
+            'image_url' => $rapper->image_url,
+            'popularity' => $rapper->popularity,
+            'followers' => $rapper->followers,
+            'attaque' => round($attaque, 2),
+            'defense' => round($defense, 2)
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\RapperController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/rappers', [RapperController::class, 'getRappers']);
+Route::get('/rapper/{id}', [RapperController::class, 'getRapper']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
